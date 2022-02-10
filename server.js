eded91b4b218acf800b53685ea565eed413a7860
Santiago <santi99oca@gmail.com>
2022-02-10 15:25:11 -0300

    Inicio del proyecto con las carpetas del MVC

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+function views(document){
+    return path.join(__dirname,"views",document)
+}
+
+//Midlewares
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req, res)=>{
+    return res.sendFile(views('index.html'))
+})
+
+app.listen(4000, ()=>{
+    console.log('Working... http://localhost:4000')
+})
